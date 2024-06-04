983307d53762b50282d4eecbafa48166839b55d0
David Ladaev <174save@gmail.com>
2024-06-04 12:56:58 +0300

    add all the svgs from figma and add new components

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-import Header from "../modules/Header/Header";
+'use client'
+import Header from '../modules/Header/Header'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
+import MobileNavbar from '../modules/MobileNavbar/MobileNavbar'
 
-
-const Layout = ({children}: {children: React.ReactNode}) => {
-    return(
-        <>
-            <Header />
-            {children}
-            <div className=""></div>
-        </>
-    )
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const isMedia800 = useMediaQuery(800)
+  return (
+    <>
+      <Header />
+      {children}
+      {isMedia800 && <MobileNavbar />}
+      <div className='' />
+    </>
+  )
 }
 
 export default Layout
